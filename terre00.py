c8e2ea5863ede1ae1f03059d8f8b05bef7daf51f
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-03-12 15:02:43 +0100

    first commit

---
+++ terre00.py
@@ -0,0 +1,10 @@
+# Exercice Coding Accelerator
+ 
+# Afficher l'alaphabet en lettre en minuscule suivir d'un retour à la ligne
+
+
+a = ord("a")
+b = a + 25
+alphabet = ascii[a:b]
+
+print(alphabet)
