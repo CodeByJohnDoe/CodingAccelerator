b8b0ea3f766d3b485ce7bc4cd0824d87f54446c9
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-03-12 16:04:01 +0100

    terre01  fini

--- terre01.py
+++ terre01.py
@@ -1,1 +1,11 @@
 # Exercice Coding Accelerator
+
+# Créer une fonction affichant son nom de fichier
+
+import os
+
+def afficher_nom_fichier():
+    result = os.path.basename(__file__)
+    print(result)
+
+afficher_nom_fichier()
