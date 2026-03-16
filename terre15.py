788c0881ea7cf1557f4c3b58cff1d26a4b3dddad
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-03-16 17:11:31 +0100

    Terre Completed

---
+++ terre15.py
@@ -0,0 +1,14 @@
+# Exercice Coding Accelerator
+ 
+# Célébration de la terre
+
+# Importation des modules
+import sys
+
+# Fonctionnement principal du programme
+def terre_celebration() :
+    if len(sys.argv) == 1:
+        print("J'ai termé l'Epreuve de la Terre et c'était prise de tête mais bon j'ai quand même réussi !")
+
+# Resultat
+terre_celebration()
