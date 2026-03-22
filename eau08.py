912942860cdab52b80f568f830580354695fc869
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-03-22 15:14:30 +0100

    eau08 fini, 5 min

---
+++ eau08.py
@@ -0,0 +1,22 @@
+# Exercice Coding Accelerator
+ 
+# Vérifier si une chaine contient uniquement des chiffres
+
+# Importation des modules
+import sys
+
+# Fonctionnement principal du programme
+def all_num() :
+    error = "false"
+    if len(sys.argv) != 2 :
+        return error 
+    argv = sys.argv[1]
+
+    # Scrutation pour avoir uniquement des chiffres
+    for i in range (len(argv)) :
+        if not (ord(argv[i]) >= 48 and ord(argv[i]) <= 57)  :
+            return error
+
+    return 'true'
+# Resultat
+print(all_num())
