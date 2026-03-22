e0f3c632cc1985d168013e700855bee443dce7f0
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-03-22 16:29:43 +0100

    eau10 fini,

--- eau10.py
+++ eau10.py
@@ -10,30 +10,14 @@ def arg_search() :
     error = "-1"
     if len(sys.argv) < 3 :
         sys.exit()
-    argv1 = str(sys.argv[1])
-    argv2 = sys.argv[2]
-    
-
-    if argv1.isdecimal() and argv2.isdecimal() :
-        argv1 = int(argv1)
-        argv2 = int(argv2)
-        delta = 0
-        min = 0
-
-        # Paramattrage de la boucle
-        if argv1 == argv2 : 
-            return error
-        if argv1 > argv2 : 
-            min = argv2
-            delta = argv1 - argv2
-        else :
-            min = argv1
-            delta = argv2 - argv1
-
-        result = [min]
-        for i in range(delta) :
-            result.append(min + i)
-    return (" ".join(map(str , result))) # Découverte de la fonction Map très interessant pour éviter les patés de boucle
+    list = sys.argv[1:-1]
+    target = sys.argv[-1]
+    if target in list :
+        for i in range(len(list)) :
+            if target == list[i] :
+                return i
+    else :
+        return error
 
 # Resultat
 print(arg_search())
