c0127b083b8e9ff2ce67283389ba93f557ea91d9
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-04-05 20:29:05 +0200

    Eau Fini,

---
+++ eau15.py
@@ -0,0 +1,16 @@
+# Exercice Coding Accelerator
+ 
+# Célébration de la fin de l'eau
+
+# Importation des modules
+
+# Importation des modules
+import sys
+
+# Fonctionnement principal du programme
+def terre_celebration() :
+    if len(sys.argv) == 1:
+        print("J'ai termé l'Epreuve de l'Eau et c'était prise de tête mais bon j'ai quand même réussi !")
+
+# Resultat
+terre_celebration()
