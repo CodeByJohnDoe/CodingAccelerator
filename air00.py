daecc0488c7757b430ba1e81babe9814916d3688
John <116635097+CodeByJohnDoe@users.noreply.github.com>
2026-04-21 19:20:04 +0200

    air00 fini, j'ai perdu du temps sur une errure d'agrument d'entrée

--- air00.py
+++ air00.py
@@ -1,31 +1,43 @@
 # Exercice Coding Accelerator
  
-# Tri à par selection croissant 
+# Séparer une chaine de charactère 
 
 # Importation des modules
 import sys
 
 # Fonctionnement principal du programme
-def tollbox_split(array, string_separator) :
-    error = "error"
-    if not array :
-        return error , sys.exit() 
-    list_array = []
+def tollbox_split(string_to_cut, string_separator=" "):
+    list_array = string_to_cut
     list_len = len(list_array)
+    # Vérification
+    if list_len == 0:
+        return "error"
 
-    # Vérification d'acquisition de nombre
     splited = []
     marker = 0
-    for i in range (list_len) :
-        current_split = []
-        if list_array [i] == string_separator :
-            current_split.append(list_array[marker:i -1 ]) 
-        if list_array [i] == list_len :
-            current_split.append(list_array[marker:i ]) 
+
+    for i in range(list_len):
+        # On cherche le séparateur
+        if list_array[i] == string_separator:
+            if i > marker:
+                splited.append(list_array[marker:i])
+            marker = i + 1
+    
+    # On ajoute le dernier mot
+    if marker < list_len:
+        splited.append(list_array[marker:])
 
     return splited
-                
-# Resultat
-string = " "
-for i in (tollbox_split(sys.argv[1:]), string) :
-    print(i)
+
+# --- Test ---
+
+# Vérification
+if len(sys.argv) < 2:
+    print("error")
+    sys.exit()
+reponse = tollbox_split(sys.argv[1])
+if reponse == "error":
+    sys.exit()
+else:
+    for mot in reponse:
+        print(mot)
